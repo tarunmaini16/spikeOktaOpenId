f5ca512de1efd9caa42e407b831997e62843f8bd
Tarun Maini <mtarun@thoughtworks.com>
2018-05-25 12:27:24 +0530

    initial

---
+++ src/main/java/com/vakt/spike/oktaopenid/OktaopenidApplication.java
@@ -0,0 +1,12 @@
+package com.vakt.spike.oktaopenid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaopenidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OktaopenidApplication.class, args);
+    }
+}
